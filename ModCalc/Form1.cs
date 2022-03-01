4372d7e39edbfdaa26f9205d2e802d91b896af29
Alexey Mason <orbweaver@mail.ru>
2022-03-01 05:19:06 +0300

    Finale

--- ModCalc/Form1.cs
+++ ModCalc/Form1.cs
@@ -10,13 +10,14 @@ namespace ModCalc
             InitializeComponent();
         }
 
-        static int Parse(string expression)
-        {
-            return CSharpScript.EvaluateAsync<int>(expression).Result;
-        }
+        //static int Parse(string expression)
+        //{
+        //    return CSharpScript.EvaluateAsync<int>(expression).Result;
+        //}
 
         public static double Evaluate(string expression)
         {
+
             System.Data.DataTable table = new System.Data.DataTable();
             table.Columns.Add("expression", string.Empty.GetType(), expression);
             System.Data.DataRow row = table.NewRow();
@@ -29,7 +30,7 @@ namespace ModCalc
             for (int x = 1; x < m; x++)
                 if (((a % m) * (x % m)) % m == 1)
                     return x;
-            return 1;
+            return 999999;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,19 +38,55 @@ namespace ModCalc
             int equasion = Convert.ToInt32(Evaluate(textBox1.Text));
             int mod = Convert.ToInt32(textBox2.Text);
 
+
+
             string subjectString = textBox1.Text;
 
             Regex Inverse = new Regex("(?<=1/)[0-9]+", RegexOptions.IgnoreCase);
             Match matchInverse = Inverse.Match(subjectString);
+            if (matchInverse.Success)
+            {
+                Regex Expression = new Regex("(1/)[0-9]+", RegexOptions.IgnoreCase);
+
+                string non = Convert.ToString(modInverse(Convert.ToInt32(matchInverse.Groups[0].Value), mod));
+                string buffer = Expression.Replace(subjectString, Convert.ToString(modInverse(Convert.ToInt32(matchInverse.Groups[0].Value), mod)));
+                int intbuffer = Convert.ToInt32(Evaluate(buffer));
+
+                if (non == "999999")
+                    textBox3.Text = "ќбратного нет";
+                else
+                    textBox3.Text = Convert.ToString(intbuffer % mod);
+            }
             //textBox3.Text = Convert.ToString(modInverse(Convert.ToInt32(matchInverse.Groups[0].Value), mod));
 
-            Regex Expression = new Regex("(?<=1/)[0-9]+", RegexOptions.IgnoreCase);
-            Match matchExpression = Inverse.Match(subjectString);
+            else
+            {
+                textBox3.Text = Convert.ToString(equasion % mod);
+            }
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9/+*-]"))
+            {
+                MessageBox.Show("¬ведите корректное выражение");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                textBox1.Focus();
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
 
-
-            textBox3.Text = Convert.ToString(equasion % mod);
-
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox2.Text, "[^0-9]"))
+            {
+                MessageBox.Show("¬ведите корректный модуль");
+                textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+                textBox2.Focus();
+                textBox2.SelectionStart = textBox2.Text.Length;
+            }
         }
     }
+
+
 }
